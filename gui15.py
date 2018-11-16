51726052bdc365bd1f2e576bb10f555c7f281c8d
ankush banik <ankushbanik1234@gmail.com>
2018-11-16 15:46:52 +0530

    file menu

--- gui15.py
+++ gui15.py
@@ -3,13 +3,17 @@ win=Tk()
 win.geometry("200x300")
 def fun1():
     print("ankush")
+def resize():
+    
+    win.geometry("200x200")
 menu=Menu(win)
 win.config(menu=menu)
 subMenu= Menu(menu)
 menu.add_cascade(label="file", menu=subMenu)
 subMenu.add_command(label="new project", command=fun1)
 subMenu.add_command(label="new file",command=fun1)
-
+subMenu.add_command(label="exit", command=quit)
+subMenu.add_command(label="resize",command=resize)
 editMenu=Menu(menu)
 menu.add_cascade(label="editing",menu=subMenu)
 editMenu.add_command(label="cut",command=fun1)
