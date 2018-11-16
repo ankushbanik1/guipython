6d7753fc11b336b68d23d4a62530594176dcaa4a
ankush banik <ankushbanik1234@gmail.com>
2018-11-16 14:27:30 +0530

    creat massege box

---
+++ gui12.py
@@ -0,0 +1,21 @@
+from tkinter import*
+import tkinter.messagebox
+root=Tk()
+tkinter.messagebox.showinfo("window title","ankush banik ")
+
+store=tkinter.messagebox.askquestion("question","do u like cily face?")
+if store=="yes":
+    print("ok u got 100")
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
